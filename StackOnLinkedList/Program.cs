c6c81b15a936d871f12a555b208296582c6bb32e
Sergei <sebudnikov@gmail.com>
2023-10-21 10:57:52 +0300

    Добавьте файлы проекта.

---
+++ StackOnLinkedList/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using StackOnLinkedList;
+
+var stack = new StackOnLinkedList<string>();
+stack.Push("Tom");
+stack.Push("Alice");
+stack.Push("Bob");
+stack.Push("Kate");
+
+foreach (var item in stack)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine();
+string header = stack.Peek();
+Console.WriteLine($"Верхушка стека: {header}");
+Console.WriteLine();
+
+header = stack.Pop();
+foreach (var item in stack)
+{
+    Console.WriteLine(item);
+}
+
---
+++ StackOnLinkedList/StackOnLinkedList.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOnLinkedList
+{
+    public class StackOnLinkedList<T> : IEnumerable<T>
+    {
+        Node<T> head;
+        int count;
+
+        public bool IsEmpty { get { return count is 0; } }
+        public int Count { get { return count; } }
+        public void Push(T data)
+        {
+            var node = new Node<T>(data);
+            node.Next = head;
+            head = node;
+            count++;
+        }
+
+        public T Pop()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("stack is empty");
+            }
+
+            var data = head.Data;
+            head = head.Next;
+            count--;
+
+            return data;
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("stack is empty");
+            }
+
+            return head.Data;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var current = head;
+
+            while (current is not null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
